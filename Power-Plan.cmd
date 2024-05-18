0a27e20f0c3e7383a27403f4a545e14ef1ec2cbb
ProgramUnwantedAlt <hornhofferd@gmail.com>
2024-05-17 20:32:12 -0700

    Update Power-Plan.cmd
    This Downloads the Ultimate Preformence plan by a fellow Github User yuk1c
    [https://github.com/yuk1c/powerplans/blob/main/UltimatePerformance.pow]
    Downloads it and attempts to automatically apply it to your system.

--- Power-Plan.cmd
+++ Power-Plan.cmd
@@ -1,3 +1,24 @@
 @echo off
 powershell -c "Invoke-WebRequest -Uri 'https://github.com/yuk1c/powerplans/raw/main/UltimatePerformance.pow' -OutFile %HOMEPATH%\AppData\Local\Temp\AdamxPowerPlan.pow
 powercfg -import "%HOMEPATH%\AppData\Local\Temp\AdamxPowerPlan.pow"
+cls
+echo Check if "Ultimate Preformance" has been installed
+powercfg /list
+echo Hit Enter If you see it (GUID FOR ME 94fa1d00-ab4b-436b-9dc1-811fbb327f27, Should be the same for you but I am unsure)
+pause>nul
+powercfg /setactive 94fa1d00-ab4b-436b-9dc1-811fbb327f27
+IF %errorlevel% NEQ 0 GOTO :error
+GOTO :end
+:error
+cls
+echo There was an error.
+echo You Have to continue Manually through Power Plan
+echo Hit the Windows Key
+echo Type "Power Plan"
+echo hit Enter
+echo change advanced power settings
+echo click the drop-down menu and click Ultimate Preformance
+echo hit Enter inside of the CMD once this is completed
+Pause>Nul
+cls
+exit
