56d84f12e411b3cde8333b1d02537d0b1506bb57
ProgramUnwantedAlt <hornhofferd@gmail.com>
2024-05-17 21:18:05 -0700

    Create Run Me.bat
    Run this to automatically run every single tweak inside of the optimization Tutorial

---
+++ Run Me.bat
@@ -0,0 +1,25 @@
+@echo off
+echo This is the Optimizer Batch file that I created for my friend. 
+echo Goto this for the full optimization Guide, 
+echo now this runs all the files inside of this folder so before continuing
+echo !!!! READ THE DESCRIPTION OF EACH FILE TO KNOW WHAT TWEAKS YOU ARE DOING TO YOUR COMPUTER !!!!
+echo DO NOT BLINDLY FUCKING RUN TWEAKS 
+echo THAT IS HOW YOU END UP WITH A BSOD LOOP AND HAVE TO RESET YOUR ENTIRE COMPUTER LOSING ALL YOUR PRECIOUS DATA
+echo Now with that out of the way 
+echo hit Enter to continue
+pause>nul 
+start 1 - Mouse Optimization.reg
+start 1 - keyboard Optimization.reg
+start 100%.reg
+start BatchOptimizations.bat
+start FPS-Optimization.reg
+start Fortnite-GameUserSettings.bat
+start Internet-Tweaks.reg
+start Power-Plan.bat
+start System Restore Point.lnk
+start XanderBaatzTweaks.reg
+echo if anything didn't run correctly
+echo I.E the Registry tweaks most likely,
+echo then Run them manually but everything in theory should work here but im tired and just finishing this up. 
+pause>nul
+exit
